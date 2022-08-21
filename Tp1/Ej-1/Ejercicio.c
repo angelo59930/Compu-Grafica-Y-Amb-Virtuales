ead6aacfdcfbd455bc51a16c63b83292ac559671
Angelo <manfrediangelosantiago10@gmail.com>
2022-08-21 12:51:33 -0300

    first ej and more content

---
+++ Tp1/Ej-1/Ejercicio.c
@@ -0,0 +1,39 @@
+
+#include <GL/glu.h>
+#include <GL/glut.h>
+
+//<<<<<<<<<<<<< Inicialización >>>>>>>>>>>>>
+void iniciar(void)
+{
+  glClearColor(1.0,1.0,1.0,0.0);
+  glColor3f(0.0f, 0.0f, 0.0f);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  gluOrtho2D(0.0, 640.0, 0.0, 480.0);
+}
+
+
+//<<<<<<<<<<<<<<<<< Dibujado >>>>>>>>>>>>>>>>
+void dibujar(void)
+{
+  glClear(GL_COLOR_BUFFER_BIT);
+
+  glFlush();
+}
+
+
+//<<<<<<<<<<<<<<<<<<< main >>>>>>>>>>>>>>>>>>
+void main(int argc, char** argv)
+{
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); 
+  glutInitWindowSize(640,480);
+  glutInitWindowPosition(100, 150); 
+  glutCreateWindow("CG Ejemplo"); 
+  glutDisplayFunc(dibujar);
+  iniciar();
+  glutMainLoop();
+
+  return 0;
+}
+
