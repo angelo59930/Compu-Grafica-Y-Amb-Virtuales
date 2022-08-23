e310f072362b0a297f43c3cf12efe501496fcec1
Angelo <manfrediangelosantiago10@gmail.com>
2022-08-23 16:43:04 -0300

    cambios

---
+++ Tp1/Ej-6/Ejercicio.cpp
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <iostream>
+#include <cmath>
+#include <GL/glu.h>
+#include <GL/glut.h>
+
+#define WIDTH 400.0
+#define HEIGTH 400.0
+
+using namespace std;
+
+struct vector2d
+{
+  float x, y;
+};
+
+//<<<<<<<<<<<<< Inicialización >>>>>>>>>>>>>
+void iniciar(void)
+{
+  glClearColor(1.0, 1.0, 1.0, 0.0);
+  glColor3f(0.0f, 0.0f, 0.0f);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  gluOrtho2D(0.0, WIDTH, 0.0, HEIGTH);
+}
+
+//<<<<<<<<<<<<<<<<< Dibujado >>>>>>>>>>>>>>>>
+void dibujar(void)
+{
+  double x = 0.0;
+
+  
+  glClear(GL_COLOR_BUFFER_BIT);
+
+
+  glutSwapBuffers();
+}
+
+//<<<<<<<<<<<<<<<<<<< main >>>>>>>>>>>>>>>>>>
+int main(int argc, char **argv)
+{
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+  glutInitWindowSize(WIDTH, HEIGTH);
+  glutInitWindowPosition(100, 150);
+  glutCreateWindow("Ejercicio-5");
+  glutDisplayFunc(dibujar);
+  iniciar();
+  glutMainLoop();
+
+  return 0;
+}
