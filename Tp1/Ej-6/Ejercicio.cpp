4ce143d78fc0fb5ec54fc70c9c9b0f77afd3ee65
Angelo <manfrediangelosantiago10@gmail.com>
2022-08-24 21:01:09 -0300

    paint medio terminado

--- Tp1/Ej-6/Ejercicio.cpp
+++ Tp1/Ej-6/Ejercicio.cpp
@@ -14,11 +14,13 @@ struct vector2d
   float x, y;
 };
 
+int r = 0, g = 0, b = 0;
+int BURSH = 5;
 //<<<<<<<<<<<<< Inicialización >>>>>>>>>>>>>
 void iniciar(void)
 {
   glClearColor(1.0, 1.0, 1.0, 0.0);
-  glColor3f(0.0f, 0.0f, 0.0f);
+  glColor3f(r, g, b);
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
   gluOrtho2D(0.0, WIDTH, 0.0, HEIGTH);
@@ -29,9 +31,41 @@ void dibujar(void)
 {
   glClear(GL_COLOR_BUFFER_BIT);
 
-  
-
   glFlush();
+}
+
+void mouse(int x, int y)
+{
+  // Generamos la brocha
+  GLint posx = x, posy = HEIGTH - y;
+  glRecti(posx, posy, posx + BURSH, posy + BURSH);
+  glRecti(posx, posy, posx + BURSH, posy + BURSH);
+  glFlush();
+}
+
+void keyboard(unsigned char key, int x, int y)
+{
+  switch (key)
+  {
+  case '+':
+    BURSH++;
+    glFlush();
+    break;
+  case '-':
+    BURSH--;
+    glFlush();
+    break;
+  case 'r':
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glFlush();
+    break;
+  case 'a':
+    r = 255;
+    g = 0;
+    b = 0;
+    break;
+  }
 }
 
 //<<<<<<<<<<<<<<<<<<< main >>>>>>>>>>>>>>>>>>
@@ -43,6 +77,8 @@ int main(int argc, char **argv)
   glutInitWindowPosition(100, 150);
   glutCreateWindow("Ejercicio-6");
   glutDisplayFunc(dibujar);
+  glutMotionFunc(mouse);
+  glutKeyboardFunc(keyboard);
   iniciar();
   glutMainLoop();
 
